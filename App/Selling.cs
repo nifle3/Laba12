8ebf4e731a6be1cfd94ccba2312334e8fe0a122a
nifie <kuper116rus.2005@gmail.com>
2023-04-20 21:39:32 +0300

    Add goods class

---
+++ App/Selling.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Selling
+    {
+        public int ID { set; get; }
+        public DateTime Date { set; get; }
+        public string Goods { set; get; }
+        public int ClientId { set; get; }
+        public int Quantity { set; get; }
+    }
+}
