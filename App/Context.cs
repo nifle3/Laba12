0ddc85591c01a173feeb381147338ef38b70aff9
nifie <kuper116rus.2005@gmail.com>
2023-04-20 22:31:04 +0300

    add dbcontext

---
+++ App/Context.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace App
+{
+    internal class Context : DbContext
+    {
+        private const string ConnectionString = "Data\r\nSource=(localdb)\\\\mssqllocaldb;Initial Catalog=LibraryDb;Integrated\r\nSecurity=True;";
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Selling> sellings { get; set; }
+
+        public Context(DbContextOptions<Context> options) :base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)=>
+            optionsBuilder.UseSqlServer(ConnectionString);
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>()
+                .HasMany(u => u.Sellings)
+                .WithOne(b => b.Client)
+                .HasForeignKey(b => b.ClientId);
+        }
+    }
+}
